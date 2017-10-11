e1022f607c6a5be8e8945f246cb20a1071063f5b
shisongsong <shi1400875096@icloud.com>
2017-10-11 18:38:30 +0800

    css

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule Exsite.LayoutView do
   use Exsite.Web, :view
+  
+  def current_user(conn) do
+    conn |> Map.get(:current_user)
+  end
+
+  def current_user?(conn) do
+    case current_user(conn) do
+      user -> true 
+      nil -> false
+    end
+  end
 end
