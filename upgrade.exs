f3d365775bd84b1f55f0966972859ad03ac4f996
shisongsong <shi1400875096@icloud.com>
2017-11-01 23:09:24 +0800

    add deploy files

---
+++ upgrade.exs
@@ -0,0 +1,8 @@
+defmodule Exsite.UpgradeCallbacks do
+  import Gatling.Bash
+
+  def before_mix_digest(env) do
+    bash("npm", ~w[install], cd: env.build_dir)
+    bash("npm", ~w[run deploy], cd: env.build_dir)
+  end
+end
