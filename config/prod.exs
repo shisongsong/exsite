8fba5ee63ce3cda26d574f7224639df415662f68
shisongsong <shi1400875096@icloud.com>
2017-11-03 15:38:36 +0800

    Setting up edeliver5

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :exsite, Exsite.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "shisongsong.top", port: 80],
+  url: [host: "www.shisongsong.top", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   root: ".",
   server: true,
